b8bec2c7e5f592d0379beefa7ca349b2f0d4e306
Bruna Freitas <brunacfreitas47@gmail.com>
2026-03-10 20:22:52 -0300

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from datetime import datetime, timedelta
 
+nome = input("Digite o nome do profissional: ")
+data_inicio = input("Digite a data do primeiro plantão (AAAA-MM-DD): ")
+
+data = datetime.strptime(data_inicio, "%Y-%m-%d")
+
+print(f"\nEscala de plantão de {nome}:\n")
+
+for i in range(10):
+    print(data.strftime("%d/%m/%Y"))
+    data += timedelta(days=2)
